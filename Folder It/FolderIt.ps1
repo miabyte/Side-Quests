b1d934f3c2ca4fbc5762eb1d066e1e674abccb07
miabyte <38174558+miabyte@users.noreply.github.com>
2025-06-17 14:40:56 +0300

    Moved FolderIt to this Repo

---
+++ Folder It/FolderIt.ps1
@@ -0,0 +1,25 @@
+﻿# Prompt the user to enter the folder directory
+$FolderPath = Read-Host "Enter the folder directory"
+
+# Check if the input is empty
+if ([string]::IsNullOrWhiteSpace($FolderPath)) {
+    Write-Host "No folder directory provided. Exiting script."
+    exit
+}
+
+# Check if the folder directory provided exists
+if (-not (Test-Path -Path $FolderPath -PathType Container)) {
+    Write-Host "Folder directory does not exist: $FolderPath"
+    exit
+}
+
+# Change the current location to the specified folder directory
+Set-Location -Path $FolderPath
+
+# Perform the same functionality within the specified folder directory
+(Get-ChildItem -File -Recurse) | ForEach-Object {
+    $Dest = New-Item -ItemType Directory -Force -Path (Join-Path $_.DirectoryName $_.BaseName)
+    $_ | Move-Item -Destination $Dest.FullName -Force
+}
+
+Write-Host "Script completed successfully!"
---
+++ Folder It/README.md
@@ -0,0 +1,14 @@
+# FolderIt✨
+Automate folder creation and file sorting. Boost productivity.
+
+![](https://i.imgur.com/DTaomtQ.gif)
+
+FolderIt is a command-line tool designed for bulk file organization. Upon execution, it prompts the user to specify a folder location. Once provided, it scans the selected folder, creates subfolders corresponding to each file's name, and moves the files into their respective folders. This process simplifies the task of organizing files in bulk, eliminating the need for manual sorting.
+
+
+![](https://i.imgur.com/T1lbrTe.gif)
+
+You can directly download the exe and use it, its a portable tool. You can check the code from FolderIt.py1 file as well. 🌿🙋‍♀️
+
+
+Made with 💖 and ☕ by MiaByte.
